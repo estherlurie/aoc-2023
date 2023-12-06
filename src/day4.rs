680fd9e491318f4e7f093382e491ceee8a950c84
elurie3 <elurie3@bloomberg.net>
2023-12-06 14:34:57 -0500

    day 4 part 2

--- src/day4.rs
+++ src/day4.rs
@@ -1,5 +1,7 @@
 #![allow(dead_code)]
 
+use std::collections::HashMap;
+
 use crate::Part;
 
 pub fn run(lines: Vec<String>, part: Part) {
@@ -31,7 +33,52 @@ fn part1(lines: Vec<String>) {
     println!("Total points: {total_points}");
 }
 
-fn part2(lines: Vec<String>) {}
+fn part2(lines: Vec<String>) {
+    let cards = get_chosen_and_winning(lines);
+    let max_card_num = cards.len();
+
+    let total_scratchcards = cards
+        .iter()
+        .enumerate()
+        .map(|(idx, cards)| (idx + 1, cards))
+        .fold(
+            HashMap::<usize, usize>::new(),
+            |mut scratchcards, (card_num, (chosen, winning))| {
+                // Get number of matches
+                let num_matches = chosen.iter().fold(0, |matches, c| {
+                    if winning.contains(c) {
+                        matches + 1
+                    } else {
+                        matches
+                    }
+                });
+                // Add scratchcards based on original
+                for num in card_num..=usize::min(card_num + num_matches, max_card_num) {
+                    scratchcards
+                        .entry(num)
+                        .and_modify(|count| {
+                            *count += 1;
+                        })
+                        .or_insert(1);
+                }
+                // Add scratchcards for each copy
+                for _ in 0..(scratchcards[&card_num] - 1) {
+                    for num in card_num + 1..=usize::min(card_num + num_matches, max_card_num) {
+                        scratchcards
+                            .entry(num)
+                            .and_modify(|count| {
+                                *count += 1;
+                            })
+                            .or_insert(1);
+                    }
+                }
+                scratchcards
+            },
+        )
+        .iter()
+        .fold(0, |total, (_, count)| total + count);
+    println!("Total scratchcards: {total_scratchcards}");
+}
 
 fn get_chosen_and_winning(lines: Vec<String>) -> Vec<(Vec<u32>, Vec<u32>)> {
     lines
