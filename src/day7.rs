2f36f6a215bfefccaa9a7a29bbc6119dae964992
elurie3 <elurie3@bloomberg.net>
2023-12-08 12:16:10 -0500

    day 7 part 2

--- src/day7.rs
+++ src/day7.rs
@@ -1,4 +1,3 @@
-use core::fmt;
 use std::collections::{HashMap, HashSet};
 
 use crate::Part;
@@ -13,35 +12,45 @@ pub fn run(lines: Vec<String>, part: Part) {
 }
 
 fn part1(lines: Vec<String>) {
+    compute_winnings::<RegularCard>(lines);
+}
+
+fn part2(lines: Vec<String>) {
+    compute_winnings::<JokerCard>(lines);
+}
+
+fn compute_winnings<C: Card + Eq + std::hash::Hash + Ord + PartialEq + PartialOrd>(
+    lines: Vec<String>,
+) {
     let mut hands = lines
         .iter()
         .map(String::as_str)
-        .map(Hand::from_str)
-        .collect::<Vec<Hand>>();
+        .map(Hand::<C>::from_str)
+        .collect::<Vec<Hand<C>>>();
     hands.sort();
     let total_winnings = hands
         .iter()
         .enumerate()
         .fold(0, |running_total, (idx, hand)| {
-            println!("{} * {}", hand, idx + 1);
             running_total + ((idx as u32 + 1) * hand.bid)
         });
     println!("Total winnings: {total_winnings}");
 }
 
-fn part2(_lines: Vec<String>) {}
-
-#[derive(Debug, Eq, PartialEq)]
-struct Hand {
-    cards: Vec<RegularCard>,
-    bid: u32,
+#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
+struct Hand<C>
+where
+    C: Card,
+{
     hand_type: Type,
+    cards: Vec<C>,
+    bid: u32,
 }
 
-impl Hand {
+impl<C: Card + Eq + std::hash::Hash + PartialEq> Hand<C> {
     fn from_str(line: &str) -> Self {
         let (cards_str, bid_str) = line.split_once(' ').unwrap();
-        let cards = cards_str.chars().map(RegularCard::from_str).collect();
+        let cards = cards_str.chars().map(Card::from_str).collect::<Vec<C>>();
         let bid = str::parse::<u32>(bid_str).unwrap();
         let hand_type = Hand::hand_type(&cards);
         Hand {
@@ -51,7 +60,67 @@ impl Hand {
         }
     }
 
-    fn hand_type(cards: &Vec<RegularCard>) -> Type {
+    fn hand_type(cards: &[C]) -> Type {
+        C::hand_type(cards)
+    }
+}
+
+#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
+enum Type {
+    HighCard,
+    OnePair,
+    TwoPair,
+    ThreeOfAKind,
+    FullHouse,
+    FourOfAKind,
+    FiveOfAKind,
+}
+
+trait Card {
+    fn from_str(c: char) -> Self
+    where
+        Self: Sized;
+    fn as_char(&self) -> char;
+    fn hand_type(cards: &[Self]) -> Type
+    where
+        Self: Sized;
+}
+
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+enum RegularCard {
+    Num(u32),
+    Jack,
+    Queen,
+    King,
+    Ace,
+}
+
+impl Card for RegularCard {
+    fn from_str(c: char) -> Self {
+        match c {
+            '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
+                RegularCard::Num(c.to_digit(10).unwrap())
+            }
+            'T' => RegularCard::Num(10),
+            'J' => RegularCard::Jack,
+            'Q' => RegularCard::Queen,
+            'K' => RegularCard::King,
+            'A' => RegularCard::Ace,
+            _ => panic!("Unknown card {c}"),
+        }
+    }
+    fn as_char(&self) -> char {
+        match self {
+            RegularCard::Num(10) => 'T',
+            RegularCard::Num(i) => char::from_digit(*i, 10).unwrap(),
+            RegularCard::Jack => 'J',
+            RegularCard::Queen => 'Q',
+            RegularCard::King => 'K',
+            RegularCard::Ace => 'A',
+        }
+    }
+
+    fn hand_type(cards: &[RegularCard]) -> Type {
         let card_set = HashSet::<&RegularCard>::from_iter(cards.iter());
         match card_set.len() {
             1 => Type::FiveOfAKind,
@@ -93,130 +162,35 @@ impl Hand {
     }
 }
 
-impl PartialOrd for Hand {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        let mut ord = self.hand_type.partial_cmp(&other.hand_type).unwrap();
-        if ord.is_eq() {
-            ord = self.cards.partial_cmp(&other.cards).unwrap();
-        }
-        Some(ord)
-    }
-}
-
-impl Ord for Hand {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.partial_cmp(other).unwrap()
-    }
-}
-
-impl fmt::Display for Hand {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "{} {} {:?}",
-            self.cards.iter().map(|c| c.as_char()).collect::<String>(),
-            self.bid,
-            self.hand_type
-        )
-    }
-}
-
-#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
-enum Type {
-    HighCard,
-    OnePair,
-    TwoPair,
-    ThreeOfAKind,
-    FullHouse,
-    FourOfAKind,
-    FiveOfAKind,
-}
-
-#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
-enum RegularCard {
-    Two,
-    Three,
-    Four,
-    Five,
-    Six,
-    Seven,
-    Eight,
-    Nine,
-    Ten,
-    Jack,
-    Queen,
-    King,
-    Ace,
-}
-
-impl RegularCard {
-    fn from_str(c: char) -> Self {
-        match c {
-            '2' => RegularCard::Two,
-            '3' => RegularCard::Three,
-            '4' => RegularCard::Four,
-            '5' => RegularCard::Five,
-            '6' => RegularCard::Six,
-            '7' => RegularCard::Seven,
-            '8' => RegularCard::Eight,
-            '9' => RegularCard::Nine,
-            'T' => RegularCard::Ten,
-            'J' => RegularCard::Jack,
-            'Q' => RegularCard::Queen,
-            'K' => RegularCard::King,
-            'A' => RegularCard::Ace,
-            _ => panic!("Unknown card {c}"),
-        }
-    }
-    fn as_char(&self) -> char {
-        match self {
-            RegularCard::Two => '2',
-            RegularCard::Three => '3',
-            RegularCard::Four => '4',
-            RegularCard::Five => '5',
-            RegularCard::Six => '6',
-            RegularCard::Seven => '7',
-            RegularCard::Eight => '8',
-            RegularCard::Nine => '9',
-            RegularCard::Ten => 'T',
-            RegularCard::Jack => 'J',
-            RegularCard::Queen => 'Q',
-            RegularCard::King => 'K',
-            RegularCard::Ace => 'A',
-        }
-    }
-}
-
 #[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 enum JokerCard {
     Joker,
-    Two,
-    Three,
-    Four,
-    Five,
-    Six,
-    Seven,
-    Eight,
-    Nine,
-    Ten,
+    Num(u32),
     Queen,
     King,
     Ace,
 }
 
 impl JokerCard {
+    fn as_regular_card(&self) -> RegularCard {
+        match self {
+            JokerCard::Num(i) => RegularCard::Num(*i),
+            JokerCard::Joker => RegularCard::Jack,
+            JokerCard::Queen => RegularCard::Queen,
+            JokerCard::King => RegularCard::King,
+            JokerCard::Ace => RegularCard::Ace,
+        }
+    }
+}
+
+impl Card for JokerCard {
     fn from_str(c: char) -> Self {
         match c {
             'J' => JokerCard::Joker,
-            '2' => JokerCard::Two,
-            '3' => JokerCard::Three,
-            '4' => JokerCard::Four,
-            '5' => JokerCard::Five,
-            '6' => JokerCard::Six,
-            '7' => JokerCard::Seven,
-            '8' => JokerCard::Eight,
-            '9' => JokerCard::Nine,
-            'T' => JokerCard::Ten,
+            '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
+                JokerCard::Num(c.to_digit(10).unwrap())
+            }
+            'T' => JokerCard::Num(10),
             'Q' => JokerCard::Queen,
             'K' => JokerCard::King,
             'A' => JokerCard::Ace,
@@ -226,18 +200,67 @@ impl JokerCard {
     fn as_char(&self) -> char {
         match self {
             JokerCard::Joker => 'J',
-            JokerCard::Two => '2',
-            JokerCard::Three => '3',
-            JokerCard::Four => '4',
-            JokerCard::Five => '5',
-            JokerCard::Six => '6',
-            JokerCard::Seven => '7',
-            JokerCard::Eight => '8',
-            JokerCard::Nine => '9',
-            JokerCard::Ten => 'T',
+            JokerCard::Num(10) => 'T',
+            JokerCard::Num(i) => char::from_digit(*i, 10).unwrap(),
             JokerCard::Queen => 'Q',
             JokerCard::King => 'K',
             JokerCard::Ace => 'A',
+        }
+    }
+
+    fn hand_type(cards: &[JokerCard]) -> Type {
+        // Initialize count map
+        let mut counts = HashMap::<&JokerCard, u8>::new();
+        for c in cards {
+            counts.entry(c).and_modify(|count| *count += 1).or_insert(1);
+        }
+        // Get joker count
+        let joker_count = counts.get(&JokerCard::Joker).unwrap_or(&0);
+
+        /*
+        ABCDE - No Joker
+        ABCDJ - 1 Joker, all diff -> 1 pair
+        AABCJ - 1 Joker, 1 pair -> 3 of a kind
+        AABBJ - 1 Joker, 2 pair -> full house
+        AAABJ - 1 Joker, 3 of a kind -> 4 of a kind
+        AAAAJ - 1 Joker, 4 of a kind -> 5 of a kind
+        ABCJJ - 2 Joker, 1 pair -> 3 of a kind
+        AABJJ - 2 Joker, 2 pair -> 4 of a kind
+        AAAJJ - 2 Joker, full house -> 5 of a kind
+        ABJJJ - 3 Joker, 3 of a kind -> 4 of a kind
+        AAJJJ - 3 Joker, full house -> 5 of a kind
+        AJJJJ - 4 Joker, 4 of a kind -> 5 of a kind
+        JJJJJ - 5 Joker, 5 of a kind
+        */
+        let regular_hand_type = RegularCard::hand_type(
+            &cards
+                .iter()
+                .map(|c| c.as_regular_card())
+                .collect::<Vec<RegularCard>>(),
+        );
+        match joker_count {
+            0 => regular_hand_type,
+            1 => match regular_hand_type {
+                Type::HighCard => Type::OnePair,
+                Type::OnePair => Type::ThreeOfAKind,
+                Type::TwoPair => Type::FullHouse,
+                Type::ThreeOfAKind => Type::FourOfAKind,
+                Type::FourOfAKind => Type::FiveOfAKind,
+                t => panic!("Invalid hand type found: {t:?}"),
+            },
+            2 => match regular_hand_type {
+                Type::OnePair => Type::ThreeOfAKind,
+                Type::TwoPair => Type::FourOfAKind,
+                Type::FullHouse => Type::FiveOfAKind,
+                t => panic!("Invalid hand type found: {t:?}"),
+            },
+            3 => match regular_hand_type {
+                Type::ThreeOfAKind => Type::FourOfAKind,
+                Type::FullHouse => Type::FiveOfAKind,
+                t => panic!("Invalid hand type found: {t:?}"),
+            },
+            4 | 5 => Type::FiveOfAKind,
+            _ => panic!("There can only be 5 cards max"),
         }
     }
 }
